3ec91e006f3a1113aa84a786b6cbac025b71b936
AurimasB <77635359+AurimasB@users.noreply.github.com>
2024-08-18 17:33:11 +0300

    Created exit button component for login page

--- frontend-project/src/routes/loginPage/LoginPage.tsx
+++ frontend-project/src/routes/loginPage/LoginPage.tsx
@@ -2,10 +2,12 @@ import "./loginPage.css";
 
 import { LoginForm } from "../../components/Forms/LoginForm/LoginForm";
 import Button from "../../components/other/Button/Button";
+import { ExitButton } from "../../components/other/ExitButton/ExitButton";
 
 export const LoginPage = ({}) => {
   return (
     <div className="loginPage">
+      <ExitButton />
       <h1 style={{ color: "white", fontSize: "3rem" }}>Sign In</h1>
       <div className="formsContainer">
         <LoginForm />
