ca33ca6e42bd53ca5cbd3b3fc03df1ada53b77fe
AurimasZen <aurimas.buta@zenitech.co.uk>
2024-08-29 14:06:49 +0300

    Added rendering protection for home page (user has to be authorized to render home page)

--- frontend-project/src/routes/header/Header.tsx
+++ frontend-project/src/routes/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 import { UserContext } from "../../context/UserContext";
 import { signOutUser } from "../../utils/firebase/firebase.utils.js";
@@ -13,24 +13,30 @@ export default function Header({ data, setResults }) {
   const { currentUser } = useContext(UserContext);
   const [username, setUsername] = useState("DEFAULT USERNAME");
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (currentUser) {
       setUsername(currentUser.email);
     }
   }, [currentUser]);
 
-  return (
-    <>
-      <header className="mainHeader">
-        <SearchBar searchData={data} setResult={setResults} />
-        <ProfileIcon userImage="/assets/icons/user.png" />
-        <Link to={"/"} style={{ all: "unset", cursor: "pointer" }}>
-          <p className="userProfile">Home page</p>
-        </Link>
-        <h3 style={{ color: "white", margin: "0px 20px" }}>{username}</h3>
-        <button onClick={signOutUser}>Sign Out</button>
-      </header>
-      <Outlet />
-    </>
-  );
+  if(currentUser){
+    return (
+      <>
+        <header className="mainHeader">
+          <SearchBar searchData={data} setResult={setResults} />
+          <ProfileIcon userImage="/assets/icons/user.png" />
+          <Link to={"/"} style={{ all: "unset", cursor: "pointer" }}>
+            <p className="userProfile">Home page</p>
+          </Link>
+          <h3 style={{ color: "white", margin: "0px 20px" }}>{username}</h3>
+          <button onClick={signOutUser}>Sign Out</button>
+        </header>
+        <Outlet />
+      </>
+    );
+  } else {
+    navigate("/guest")
+  }
 }
