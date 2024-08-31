7d4cf826d737e219b761ecf29dab4d8e01755790
AurimasZen <aurimas.buta@zenitech.co.uk>
2024-08-31 16:42:11 +0300

    Partially did hover styling for user menu component

--- frontend-project/src/components/header/ProfileIcon/ProfileIcon.tsx
+++ frontend-project/src/components/header/ProfileIcon/ProfileIcon.tsx
@@ -8,14 +8,14 @@ export default function ProfileIcon({ userImage }) {
       <Link to={"myProfile"} className="profileIcon">
         <img src={userImage} alt="An icon of a person's profile view" />
       </Link>
-      <ul className="dropdownMenu">
+      <div className="dropdownMenu">
         <div className="userDescription">
           <p>aurimas.buta@zenitech.co.uk</p>
         </div>
-       <Link to={"/profile"} className="dropdownItem"><li>User Profile</li></Link>
-       <Link to={"/profile"} className="dropdownItem"><li>Settings</li></Link>
-       <Link to={"/profile"} className="dropdownItem"><li>Logout</li></Link>
-      </ul>
+       <Link to={"/profile"} className="dropdownItem"><p>User Profile</p></Link>
+       <Link to={"/profile"} className="dropdownItem"><p>Settings</p></Link>
+       <Link to={"/profile"} className="dropdownItem"><p>Logout</p></Link>
+      </div>
     </div>
   );
 }
