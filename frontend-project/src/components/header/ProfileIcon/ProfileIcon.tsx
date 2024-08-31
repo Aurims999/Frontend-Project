0df0c3dca67c2bb0ec3f2b75837ff80c4881b20f
AurimasZen <aurimas.buta@zenitech.co.uk>
2024-08-31 19:07:55 +0300

    Added listener logic to the dropdown menu so that if user clicks anywhere else on the page, the menu should dissapear

--- frontend-project/src/components/header/ProfileIcon/ProfileIcon.tsx
+++ frontend-project/src/components/header/ProfileIcon/ProfileIcon.tsx
@@ -1,14 +1,38 @@
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "./ProfileIcon.css";
 
 export default function ProfileIcon({ userImage }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownMenu = useRef(null);
+  const imageIcon = useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if(dropdownMenu.current && !dropdownMenu.current.contains(event.target) && !imageIcon.current.contains(event.target) ){
+        toggleMenu();
+        document.removeEventListener("mousedown", handleClickOutside);
+      }
+    }
+
+      if(isOpen){
+        document.addEventListener("mousedown", handleClickOutside);
+      } 
+
+  }, [dropdownMenu, isOpen])
+
   return (
-    <div className="userManagementButton">
-      <Link to={"myProfile"} className="profileIcon">
-        <img src={userImage} alt="An icon of a person's profile view" />
-      </Link>
-      <div className="dropdownMenu">
+    <div className="userManagementButton" ref={imageIcon}>
+      <img src={userImage} 
+           alt="An icon of a person's profile view" 
+           className="profileIcon"
+           onClick={toggleMenu} />
+      <div className="dropdownMenu" style={{ display: isOpen ? "block" : "none" }} ref={dropdownMenu}>
         <div className="userDescription">
           <p>aurimas.buta@zenitech.co.uk</p>
         </div>
