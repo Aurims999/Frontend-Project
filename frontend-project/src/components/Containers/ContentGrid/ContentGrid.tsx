136c5a6e1d5ed028a95800fb3ac2ec600723ac42
AurimasZen <aurimas.buta@zenitech.co.uk>
2024-09-09 15:07:52 +0300

    Created NoData component

--- frontend-project/src/components/Containers/ContentGrid/ContentGrid.tsx
+++ frontend-project/src/components/Containers/ContentGrid/ContentGrid.tsx
@@ -1,15 +1,21 @@
+import { NoData } from "../../other/NoData/NoData";
 import "./contentGrid.css";
 
 export const ContentGrid = ({ title = "", children, amountOfColumns = 5 }) => {
   return (
     <section className="containers-grid">
       <h2 className="gridTitle">{title}</h2>
-      <div
-        className="contentGrid"
-        style={{ gridTemplateColumns: `repeat(${amountOfColumns}, 1fr)` }}
-      >
-        {children}
-      </div>
+      {children.length > 0 ? (
+         <div
+         className="contentGrid"
+         style={{ gridTemplateColumns: `repeat(${amountOfColumns}, 1fr)` }}
+         >
+         {children}
+       </div>
+      ) : (
+        <NoData/>
+      )}
+
     </section>
   );
 };
