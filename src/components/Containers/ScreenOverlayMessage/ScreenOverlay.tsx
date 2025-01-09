2f390a4fe5981a2e13f4c687465d049d420b49f5
AurimasZen <aurimas.buta@zenitech.co.uk>
2025-01-09 11:54:31 +0200

    Added scroll disabling logic to screen overlay's render logic

--- src/components/Containers/ScreenOverlayMessage/ScreenOverlay.tsx
+++ src/components/Containers/ScreenOverlayMessage/ScreenOverlay.tsx
@@ -5,12 +5,11 @@ import "./screenOverlay.css"
 export const ScreenOverlay = ({children}) => {
     useEffect(() => {
         document.body.classList.add("scroll-disabled");
-
         return () => {
             document.body.classList.remove("scroll-disabled");
         }
     }, []);
-    
+
     return(
         <div className="screenOverlay">
             {children}
